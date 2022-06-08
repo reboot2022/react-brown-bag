9e219cc875ae852bb60cc907cee72c3db9c0d2c5
Rafee Jenkins <rafeejenkins@gmail.com>
2022-06-08 16:04:59 +0100

    Updated .gcloudignore

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Hello() {
+    // Creates a state called name, and a setter called setName.
     const [name, setName] = useState("")
     
+    // This function handles input to the text field.
     const handleChange = (event) => {
         const value = event.target.value
         setName(value)
     }
 
+    // useEffect calls the callback function when the name changes.
     useEffect(() => {
         if (name.length >= 10) {
             alert("Your name is too long, we don't have unlimited storage you know!")
@@ -15,6 +18,7 @@ export default function Hello() {
         }
     }, [name])
 
+    // useEffect calls the callback function on component render
     useEffect(() => {
         alert("Welcome to my page!")
     }, [])
